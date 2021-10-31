799697b63a542e5e30b83e9b0777d77882be1b89
Baptiste Langlade <baptouuuu@gmail.com>
2021-10-31 11:56:22 +0100

    Initial Commit

---
+++ Innmind/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Innmind
+//
+//  Created by Baptouuuu on 31/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Innmind/InnmindApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnmindApp.swift
+//  Innmind
+//
+//  Created by Baptouuuu on 31/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct InnmindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
