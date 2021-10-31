b95f1a3e89975d11fd801e6fb467577f4eaaf4fa
Baptiste Langlade <baptouuuu@gmail.com>
2021-10-31 14:59:11 +0100

    first layout

--- Innmind/ContentView.swift
+++ Innmind/ContentView.swift
@@ -7,10 +7,83 @@
 
 import SwiftUI
 
+struct Package: Hashable {
+    let name: String
+}
+
 struct ContentView: View {
+    @State var currentPackage = 0
+
+    var packages: [Package] = [
+        .init(name: "Innmind"),
+        .init(name: "immutable"),
+        .init(name: "time-continuum"),
+        .init(name: "filesystem"),
+    ]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ListView(
+                packages: packages,
+                selectedPackage: $currentPackage
+            )
+                .frame(minWidth: 100)
+
+            switch currentPackage {
+            case 0:
+                VendorView()
+            default:
+                MainView(package: self.packages[currentPackage])
+            }
+        }
+        .frame(minWidth: 600, minHeight: 400)
+    }
+}
+
+struct ListView : View {
+    let packages: [Package]
+    @Binding var selectedPackage: Int
+
+    var body: some View {
+        ScrollView {
+            let current = packages[selectedPackage]
+            ForEach(packages.indices, id: \.self) { index in
+                HStack {
+                    Text(packages[index].name)
+                        .foregroundColor(current == packages[index] ? Color.primary : Color.secondary)
+                    Spacer()
+                }
+                .padding(.vertical, 5)
+                .padding(.horizontal, 10)
+                .onTapGesture {
+                    selectedPackage = index
+                }
+                Divider()
+            }
+            Spacer()
+        }
+    }
+}
+
+struct MainView : View {
+    let package: Package
+
+    var body: some View {
+        VStack {
+            Text("innmind/"+self.package.name).font(.title)
+            Spacer()
+        }
+        .padding(5)
+    }
+}
+
+struct VendorView : View {
+    var body: some View {
+        VStack {
+            Text("Innmind").font(.title)
+            Text("vendor graph here")
+            Spacer()
+        }
     }
 }
 
